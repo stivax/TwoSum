c8968b38493c9d339d6ae204e7926b51613943b9
stivax <stivax@gmail.com>
2022-04-29 21:51:24 +0300

    public to private

--- src/edu/lessons/TwoSum.java
+++ src/edu/lessons/TwoSum.java
@@ -12,7 +12,7 @@ public class TwoSum {
 
     }
 
-    public static int[] twoSum(int[] nums, int target) {
+    private static int[] twoSum(int[] nums, int target) {
         int[] res = new int[2];
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums.length; j++) {
